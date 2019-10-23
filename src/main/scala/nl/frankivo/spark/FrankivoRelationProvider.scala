4b2c5d116195497793ac3c9bfbb6b0e7d0fb5f80
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2019-10-23 10:52:59 +0200

    SchemaRelationProvider

--- src/main/scala/nl/frankivo/spark/FrankivoRelationProvider.scala
+++ src/main/scala/nl/frankivo/spark/FrankivoRelationProvider.scala
@@ -1,7 +1,11 @@
 package nl.frankivo.spark
 
-import org.apache.spark.sql.sources.DataSourceRegister
+import org.apache.spark.sql.SQLContext
+import org.apache.spark.sql.sources.{BaseRelation, DataSourceRegister, SchemaRelationProvider}
+import org.apache.spark.sql.types.StructType
 
-class FrankivoRelationProvider extends DataSourceRegister {
+class FrankivoRelationProvider extends DataSourceRegister with SchemaRelationProvider {
   override def shortName(): String = "frankivo"
+
+  override def createRelation(sqlContext: SQLContext, parameters: Map[String, String], schema: StructType): BaseRelation = ???
 }
