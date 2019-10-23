1a9c7056d945a27ca308c1929116c423fe4a081b
Frank Oosterhuis <frank.oosterhuis@getthere.nl>
2019-10-23 11:13:01 +0200

    PrunedFilteredScan

--- src/main/scala/nl/frankivo/spark/FrankivoRelationProvider.scala
+++ src/main/scala/nl/frankivo/spark/FrankivoRelationProvider.scala
@@ -1,21 +1,31 @@
 package nl.frankivo.spark
 
-import org.apache.spark.sql.SQLContext
-import org.apache.spark.sql.sources.{BaseRelation, DataSourceRegister, SchemaRelationProvider}
+import org.apache.spark.{Partition, TaskContext}
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.{Row, SQLContext}
+import org.apache.spark.sql.sources.{BaseRelation, DataSourceRegister, Filter, PrunedFilteredScan, SchemaRelationProvider}
 import org.apache.spark.sql.types.StructType
 
 class FrankivoRelationProvider extends DataSourceRegister with SchemaRelationProvider {
   override def shortName(): String = "frankivo"
 
   override def createRelation(
-                               sqlContext: SQLContext,
-                               parameters: Map[String, String],
+                               _sqlContext: SQLContext,
+                               _parameters: Map[String, String],
                                _schema: StructType
                              ): BaseRelation = {
-    new BaseRelation {
-      override def sqlContext: SQLContext = ???
+    new BaseRelation with PrunedFilteredScan {
+      override def sqlContext: SQLContext = _sqlContext
 
       override def schema: StructType = _schema
+
+      override def buildScan(requiredColumns: Array[String], filters: Array[Filter]): RDD[Row] =
+        new RDD[Row](sqlContext.sparkContext, Nil) {
+          override def compute(split: Partition, context: TaskContext): Iterator[Row] = ???
+
+          override protected def getPartitions: Array[Partition] = ???
+        }
     }
   }
+
 }
